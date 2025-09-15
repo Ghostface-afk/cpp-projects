877469ce357279fe4a0062c4bc34f5c7f2d5d89f
Ghostface-afk <lawdanga620@gmail.com>
2025-09-15 13:48:16 +0300

    classwork

---
+++ c++/discount.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+float discount(float principal, int rate);
+//float final_price;
+
+int main(){
+    float amount;
+    float discount;
+    float final_price;
+    cout << "Enter purchase amount: ";
+    cin >> amount;
+
+    if(amount >= 5000){
+        discount = amount * 0.1;
+        final_price = amount- discount;
+        cout << "Final price after discount: " << final_price << endl;
+        cout << "Your discount is: " << discount << endl;
+    }
+    else if(amount >= 1000){
+        discount = amount * 0.05;
+        final_price = amount - discount;
+        cout << "Final price after discount: " << final_price << endl;
+        cout << "Your discount is: " << discount << endl;
+    }
+    else{
+        cout<<"No discount applicable"<<endl;
+    }
+    return 0;
+}
+
+float discount (float principal, int rate){
+
+    float principal;
+    float discount;
+}
--- c++/interestcalculator.cpp
+++ c++/interestcalculator.cpp
@@ -9,18 +9,58 @@ using namespace std;
 
 // Function prototype
 float s_interest;
+float discount(float principal, float final_price);
 float simple_interest(float principal, int rate, int time);
 
 int main(){
     //function call
-    s_interest = simple_interest(20000.0 , 5, 2);
+    float principal;
+    float final_price;
+    float discount;
+    cout << "Enter principal amount: ";
+    cin >> principal;
+
+    int rate;
+    cout << "Enter rate of interest: ";
+    cin >> rate;
+
+    int time;
+    cout << "Enter time: ";
+    cin >> time;
+
+    s_interest = simple_interest(principal, rate, time);
     cout << "Simple Interest is: " << s_interest << endl;
     return 0;
 }
 
+
 //funtion definition
 float simple_interest(float principal, int rate, int time){
-    float interest;
-    interest = principal * rate * time / 100;
-    return interest;
+    float simple_interest;
+    simple_interest = principal * rate/100 * time;
+    return simple_interest;
 }
+
+//discount 
+float discount(float principal, float final_price){
+     
+    float discount;
+     if(principal >= 5000){
+        discount = principal * 0.1;
+        final_price = principal - discount;
+    }
+    else if(principal >= 1000){
+        discount = principal * 0.05;
+        final_price = principal - discount;
+    }
+    else{
+        cout<<"No discount applicable"<<endl;
+    }
+
+     cout << "Final price after discount: " << final_price << endl;
+     cout<<"Discount is: "<< discount <<endl;
+
+
+     return discount;
+
+     }
