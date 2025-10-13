feee76b470e121dcd7aa4cc615b655488869bd02
Ghostface-afk <lawdanga620@gmail.com>
2025-10-13 13:32:28 +0300

    Cat 2

--- librarymanagementsystem.cpp
+++ librarymanagementsystem.cpp
@@ -1,74 +1,68 @@
-//Lawrence Odanga
-//Admission number: BCS-03-0052/2025
-//Description: CAT2 - Library Management System using Inheritance
-
 #include <iostream>
 using namespace std;
 
+// Base class
 class Person {
-    protected:
-        string name;
-    
-    public:
-        void setName(string n){ // constructor
-            name = n;
-        }
-        string getName(){
-            return getName();
-        }
+protected:
+    string name;
+
+public:
+    void setName(string n) {
+        name = n;
+    }
+
+    string getName() {
+        return name;
+    }
 };
 
-class LibraryMember: public Person {
-    private:
-        int memberID;
-    private:
-        int booksBorrowed;
+// Derived class 1
+class LibraryMember : public Person {
+private:
+    int memberID;
+    int booksBorrowed;
+
 public:
-LibraryMember(string n, int m, int b){ // constructor
-    
+    LibraryMember(string n, int m, int b) {
         setName(n);
         memberID = m;
         booksBorrowed = b;
-};
+    }
 
-public:
-        int getName(){
-            cout << "Your name is " << getName() << endl;
-            return getName();
-        }
-
-    int getMemberID(){
-        cout << "Your ID number is "<< getMemberID() << endl;
+    int getMemberID() {
         return memberID;
     }
-    int getBooksBorrowed(){
-        cout << "The total books borrowed is " << getBooksBorrowed() <<endl;
+
+    int getBooksBorrowed() {
         return booksBorrowed;
     }
 };
-class PremiumMember:public LibraryMember {
-    private: 
-        double membershipFee;
-        
+
+// Derived class 2
+class PremiumMember : public LibraryMember {
+private:
+    double membershipFee;
+
 public:
-PremiumMember(string n, int m, int b,double memFee){
-    membershipFee = memFee;
-        int getMembershipFee(){
-            cout << "Your membership fees are " << getMembershipFee() <<endl;
-            return membershipFee;
-        }
+    PremiumMember(string n, int m, int b, double fee)
+        : LibraryMember(n, m, b)  // Initialize base class
+    {
+        membershipFee = fee;
+    }
+
+    double getMembershipFee() {
+        return membershipFee;
     }
 };
 
+int main() {
+    // Creating an object for PremiumMember
+    PremiumMember userinfo("Lawrence Odanga", 12345, 12, 1600);
 
-int main()
-{
-//Creating an object for PremiumMember
-PremiumMember userinfo("Lawrence Odanga",12345 ,12,1600);
+    cout << "Member's Name: " << userinfo.getName() << endl;
+    cout << "Member's ID: " << userinfo.getMemberID() << endl;
+    cout << "Books Borrowed: " << userinfo.getBooksBorrowed() << endl;
+    cout << "Membership Fee: " << userinfo.getMembershipFee() << " shillings" << endl;
 
-cout<<"Member's name: "<<userinfo.getName()<<endl;
-cout<<"Member's ID:"<<userinfo.getMemberID()<<endl;
-cout<<"You have borrowed "<<userinfo.getBooksBorrowed()<<" books"<<endl;
-cout<<"Your Membership Fee is:"<<userinfo.getMembershipFee()<<" shillings"<<endl;
-return 0;
+    return 0;
 }
