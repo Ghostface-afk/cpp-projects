106e479503fe387d41a2a545c9cad2f3345e0cf3
Ghostface-afk <lawdanga620@gmail.com>
2025-10-25 12:41:07 +0300

    add

---
+++ area.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area, perimeter;
+    float pi=3.142;
+    printf("Enter radius: ");
+    scanf("%f", &radius);
+
+    area = pi * radius * radius;
+    perimeter = 2 * pi * radius;
+    printf("Area = %.2f\n", area);
+    printf("Perimeter = %.2f\n", perimeter);
+
+    return 0;
+}
